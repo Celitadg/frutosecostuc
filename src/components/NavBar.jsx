9a4884f416632d560fcc43bb32cd5174d8b93054
Celia De Gregorio <celitadg@gmail.com>
2021-08-01 18:02:43 -0300

    Desafio Clase 3

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+
+
+export default function NavBar() {
+    return(
+        <header>
+            <h1><a href="#">Frutos Secos Tuc</a></h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Productos</a>
+            </nav>
+        </header>
+    )
+}
