4a2004544783f1317663687fa642fb2266e3ba5b
Celia De Gregorio <celitadg@gmail.com>
2021-08-17 12:27:06 -0300

    Desafio Clase 6

---
+++ src/components/Api/Products.jsx
@@ -0,0 +1,62 @@
+
+
+export  const products = [
+                {
+                    id: 1,
+                    title: "Sport Mix sin pasas",
+                    description: "Nueces, almendras, castañas de cajú, maní, chips de banana",
+                    price:"$840/Kg",
+                },
+                {
+                    id: 2,
+                    title: "Sport Mix",
+                    description: "Nueces, almendras, castañas de cajú, maní, chips de banana, pasas de uva morochas",
+                    price: "$610/Kg",
+                },
+                {
+                    id: 3,
+                    title: "Mix Mediterraneo sin pasas",
+                    description: "Nueces, almendras, castañas de cajú, maní",
+                    price: "$830/Kg",
+                },
+                {
+                    id: 4,
+                    title: "Mix Mediterraneo",
+                    description: "Nueces, almendras, castañas de cajú, maní, pasas de uva rubias, pasas de uva morochas",
+                    price: "$690/Kg",
+                },
+                {
+                    id: 5,
+                    title: "Mix Clásico + Cereales",
+                    description: "Nueces, almendras, maní, pasas de uva morochas, copos sin azúcar",
+                    price: "$490/Kg",
+                },
+                {
+                    id: 6,
+                    title: "Mix Clásico",
+                    description: "Nueces, almendras, maní, pasas de uva morochas",
+                    price: "$510/Kg",
+                },
+                {
+                    id: 7,
+                    title: "Snack Mix Premium",
+                    description: "Nueces, almendras, castañas de cajú, pasas de uva rubias, pasas de uva morochas",
+                    price: "$790/Kg",
+                },
+                {
+                    id: 8,
+                    title: "Snack Mix Especial",
+                    description: "Nueces, almendras, pasas de uva rubias, pasas de uva morochas",
+                    price: "$740/Kg",
+                },
+                {
+                    id: 9,
+                    title: "Snack Mix",
+                    description: "Nueces, pasas de uva rubias, pasas de uva morochas",
+                    price: "$680/Kg",
+                },
+                
+            ]
+
+
+
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,71 +1,27 @@
+import { useState, useEffect } from "react"
 import Item from "./Item"
+import {products} from "./Api/Products"
+
 
 
 export default function ItemList() {
     
-    const items = [
-        {
-            id: 1,
-            title: "Sport Mix sin pasas",
-            description: "Nueces, almendras, castañas de cajú, maní, chips de banana",
-            price:"$840/Kg",
-        },
-        {
-            id: 2,
-            title: "Sport Mix",
-            description: "Nueces, almendras, castañas de cajú, maní, chips de banana, pasas de uva morochas",
-            price: "$610/Kg",
-        },
-        {
-            id: 3,
-            title: "Mix Mediterraneo sin pasas",
-            description: "Nueces, almendras, castañas de cajú, maní",
-            price: "$830/Kg",
-        },
-        {
-            id: 4,
-            title: "Mix Mediterraneo",
-            description: "Nueces, almendras, castañas de cajú, maní, pasas de uva rubias, pasas de uva morochas",
-            price: "$690/Kg",
-        },
-        {
-            id: 5,
-            title: "Mix Clásico + Cereales",
-            description: "Nueces, almendras, maní, pasas de uva morochas, copos sin azúcar",
-            price: "$490/Kg",
-        },
-        {
-            id: 6,
-            title: "Mix Clásico",
-            description: "Nueces, almendras, maní, pasas de uva morochas",
-            price: "$510/Kg",
-        },
-        {
-            id: 7,
-            title: "Snack Mix Premium",
-            description: "Nueces, almendras, castañas de cajú, pasas de uva rubias, pasas de uva morochas",
-            price: "$790/Kg",
-        },
-        {
-            id: 8,
-            title: "Snack Mix Especial",
-            description: "Nueces, almendras, pasas de uva rubias, pasas de uva morochas",
-            price: "$740/Kg",
-        },
-        {
-            id: 9,
-            title: "Snack Mix",
-            description: "Nueces, pasas de uva rubias, pasas de uva morochas",
-            price: "$680/Kg",
-        },
-        
-    ]
+    const [productos, setProductos] = useState([])
+    
+    useEffect(() => {
+        const itemList = () => {
+            return new Promise ((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(products)
+                }, 2000)
+            })
+        }
+        itemList().then((prod) => setProductos(prod))
+    }, [productos])
 
-
-    const itemList = items.map(item => <Item title={ item.title } description={ item.description } price={ item.price }/>)
     return(
         <>
-            {itemList}
+        { productos.map(item => <Item key={item.id} title={item.title} description={item.description} price={item.price} />) }
         </>
     )
 }
